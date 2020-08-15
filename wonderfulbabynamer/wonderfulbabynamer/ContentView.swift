db657991dcdd23e5de148104fa6c713c450bc3a9
Victoria Heric <victoriah_2000@yahoo.com>
2020-08-15 14:40:30 -0700

    A app is born.

---
+++ wonderfulbabynamer/wonderfulbabynamer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wonderfulbabynamer
+//
+//  Created by Victoria Heric on 8/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wonderfulbabynamer/wonderfulbabynamer/wonderfulbabynamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wonderfulbabynamerApp.swift
+//  wonderfulbabynamer
+//
+//  Created by Victoria Heric on 8/15/20.
+//
+
+import SwiftUI
+
+@main
+struct wonderfulbabynamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
